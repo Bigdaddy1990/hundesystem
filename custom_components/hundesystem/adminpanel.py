affef5d292589b3f0b276c0e1d31c22182d20677
Bigdaddy1990 <91220458+Bigdaddy1990@users.noreply.github.com>
2025-07-02 00:34:34 +0200

    Create adminpanel.py

---
+++ custom_components/hundesystem/adminpanel.py
@@ -0,0 +1,8 @@
+def get_adminpanel(hass):
+    return {
+        "title": "Hundesystem",
+        "icon": "mdi:dog",
+        "path": "hundesystem_admin",
+        "url_path": "/lovelace/hundesystem_admin",
+        "panel": True
+    }
