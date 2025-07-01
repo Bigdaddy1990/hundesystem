cb64c1fd453cfcd5b0555eafee55616817b03c85
Bigdaddy1990 <91220458+Bigdaddy1990@users.noreply.github.com>
2025-07-02 00:44:07 +0200

    Update adminpanel.py

--- custom_components/hundesystem/adminpanel.py
+++ custom_components/hundesystem/adminpanel.py
@@ -1,8 +1,47 @@
-def get_adminpanel(hass):
-    return {
-        "title": "Hundesystem",
-        "icon": "mdi:dog",
-        "path": "hundesystem_admin",
-        "url_path": "/lovelace/hundesystem_admin",
-        "panel": True
-    }
+title: Hundesystem Admin
+path: hundesystem_admin
+icon: mdi:cog-outline
+type: custom:vertical-stack
+cards:
+  - type: markdown
+    content: >
+      # 🐶 Hundesystem Adminpanel  
+      Übersicht über alle konfigurierten Hunde, Status & Aktionen
+
+  - type: entities
+    title: Hund: Rex
+    entities:
+      - entity: input_boolean.rex_feeding_morning
+        name: Frühstück erledigt
+      - entity: input_boolean.rex_feeding_evening
+        name: Abendessen erledigt
+      - entity: counter.rex_feeding_morning
+        name: Frühstücks-Zähler
+      - entity: counter.rex_outside
+        name: Gassi-Zähler
+      - entity: input_boolean.rex_visitor_mode
+        name: Besucherhund-Modus
+
+  - type: custom:mushroom-template-card
+    icon: mdi:dog-service
+    primary: Rückfrage starten
+    tap_action:
+      action: call-service
+      service: script.rex_trigger_feeding_check
+      data: {}
+
+  - type: custom:mushroom-template-card
+    icon: mdi:bell-ring
+    primary: Push-Test senden
+    tap_action:
+      action: call-service
+      service: script.rex_push_test
+      data: {}
+
+  - type: custom:mushroom-template-card
+    icon: mdi:restart
+    primary: Tagesstatistik zurücksetzen
+    tap_action:
+      action: call-service
+      service: script.rex_daily_reset
+      data: {}
