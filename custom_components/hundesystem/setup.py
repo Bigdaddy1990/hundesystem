9035682542672f277d96a5368e147160103c6650
Bigdaddy1990 <91220458+Bigdaddy1990@users.noreply.github.com>
2025-07-02 00:38:46 +0200

    Update setup.py

--- custom_components/hundesystem/setup.py
+++ custom_components/hundesystem/setup.py
@@ -1,19 +1,18 @@
 # custom_components/hundesystem/setup.py
 from homeassistant.core import HomeAssistant
-from homeassistant.helpers import entity_registry as er
-from homeassistant.helpers.template import Template
-from homeassistant.helpers.entity_component import async_update_entity
-from homeassistant.helpers.entity_platform import async_get_platforms
-from homeassistant.util import slugify
-from datetime import time
 from homeassistant.helpers.event import async_track_time_change
+from homeassistant.helpers.entity_registry import async_get as async_get_registry
+from homeassistant.helpers.area_registry import async_get as async_get_area_registry
+from homeassistant.helpers.device_registry import async_get as async_get_device_registry
+from homeassistant.helpers.entity_platform import async_get_platforms
+from homeassistant.helpers import entity_component
+from datetime import time
 
-DOMAIN = "hundesystem"
-
-INPUT_BOOLEAN_PLATFORM = "input_boolean"
-COUNTER_PLATFORM = "counter"
+INPUT_BOOLEAN_DOMAIN = "input_boolean"
+COUNTER_DOMAIN = "counter"
 
 async def async_create_helpers(hass: HomeAssistant, name: str):
+    """Erstellt alle erforderlichen Entitäten dynamisch, falls sie nicht existieren."""
     suffixes_boolean = [
         "feeding_morning",
         "feeding_lunch",
@@ -31,34 +30,29 @@ async def async_create_helpers(hass: HomeAssistant, name: str):
         "outside"
     ]
 
-    # Input Booleans
     for suffix in suffixes_boolean:
-        entity_id = f"input_boolean.{name}_{suffix}"
+        entity_id = f"{INPUT_BOOLEAN_DOMAIN}.{name}_{suffix}"
         if not hass.states.get(entity_id):
             await hass.services.async_call(
-                "input_boolean", "toggle",
+                INPUT_BOOLEAN_DOMAIN,
+                "turn_on",
                 {"entity_id": entity_id},
                 blocking=False,
             )
+            hass.states.async_set(entity_id, "off", {"friendly_name": suffix.replace("_", " ").capitalize()})
 
-    # Counter
     for suffix in suffixes_counter:
-        entity_id = f"counter.{name}_{suffix}"
+        entity_id = f"{COUNTER_DOMAIN}.{name}_{suffix}"
         if not hass.states.get(entity_id):
-            await hass.services.async_call(
-                "counter", "reset",
-                {"entity_id": entity_id},
-                blocking=False,
-            )
+            hass.states.async_set(entity_id, "0", {
+                "friendly_name": f"{suffix.replace('_', ' ').capitalize()} Counter",
+                "unit_of_measurement": "x"
+            })
 
-    # Counter Reset um 23:59
     async def reset_counters(now):
         for suffix in suffixes_counter:
-            counter_id = f"counter.{name}_{suffix}"
-            if hass.states.get(counter_id):
-                await hass.services.async_call(
-                    "counter", "reset", {"entity_id": counter_id},
-                    blocking=False
-                )
+            entity_id = f"{COUNTER_DOMAIN}.{name}_{suffix}"
+            if hass.states.get(entity_id):
+                await hass.services.async_call(COUNTER_DOMAIN, "reset", {"entity_id": entity_id}, blocking=False)
 
     async_track_time_change(hass, reset_counters, hour=23, minute=59, second=0)
